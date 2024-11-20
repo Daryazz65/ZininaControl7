80d4225a7f9b79c3adc83d4ed4eb420d7400adb2
Дарья Зинина <zininadashuta167@gmail.com>
2024-11-20 12:36:40 +0300

    Добавьте файлы проекта.

---
+++ ZininaControl7/View/Window/MenuWindow.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace ZininaControl7.View.Window
+{
+    /// <summary>
+    /// Логика взаимодействия для MenuWindow.xaml
+    /// </summary>
+    public partial class MenuWindow : System.Windows.Window
+    {
+        public MenuWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void AddMaterialBtn_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void AddManufactuer_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void AddEmployeeBtn_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void AccountingBtn_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void OtchetBtn_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
